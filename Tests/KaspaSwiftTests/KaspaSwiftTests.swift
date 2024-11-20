093dec185c234fbf8b928aecfccf54c763eba410
xueyuejie <358838943@qq.com>
2024-11-20 13:55:22 +0800

    Update KaspaSwiftTests.swift

--- Tests/KaspaSwiftTests/KaspaSwiftTests.swift
+++ Tests/KaspaSwiftTests/KaspaSwiftTests.swift
@@ -11,38 +11,38 @@ final class KaspaSwiftTests: XCTestCase {
     }
     
     func testAccountExample() throws {
-        do {
-            let mkey = KaspaKey.fromMnemonics("")!
-            var hdVer = HDNode.HDversion()
-            hdVer.publicPrefix = pubKeyPrefix
-            hdVer.privatePrefix = privatePrefix
-            let mMasterPubKey = mkey.serializePublicKey(version: hdVer)
-            let account = try mkey.derive(path: "44'/111111'/0'")
-//            let akey = try mroot.derive(path: "0'")
-            let kpub = account.serializePublicKeyString(version: hdVer)
-            // kpub 通过
-            
-            let node = try account.derive(path: "0").derive(path: "0")
-            
-//            let hdnode = node?.derive(path: "0/0", derivePrivateKey: false)
-            let addressService = KaspaAddressService(isTestnet: false)
-            let address = try addressService.makeAddress(for: node.publicKey)
-            
-            debugPrint(address)
-            debugPrint(address)
-        } catch let error {
-            debugPrint(error)
-        }
+//        do {
+//            let mkey = KaspaKey.fromMnemonics("")!
+//            var hdVer = HDNode.HDversion()
+//            hdVer.publicPrefix = pubKeyPrefix
+//            hdVer.privatePrefix = privatePrefix
+//            let mMasterPubKey = mkey.serializePublicKey(version: hdVer)
+//            let account = try mkey.derive(path: "44'/111111'/0'")
+////            let akey = try mroot.derive(path: "0'")
+//            let kpub = account.serializePublicKeyString(version: hdVer)
+//            // kpub 通过
+//            
+//            let node = try account.derive(path: "0").derive(path: "0")
+//            
+////            let hdnode = node?.derive(path: "0/0", derivePrivateKey: false)
+//            let addressService = KaspaAddressService(isTestnet: false)
+//            let address = try addressService.makeAddress(for: node.publicKey)
+//            
+//            debugPrint(address)
+//            debugPrint(address)
+//        } catch let error {
+//            debugPrint(error)
+//        }
     }
     
     func testGrpcExample() async throws {
-        let client = KaspaClient(host: "47.238.220.71", port: 16110)
-//        try KaspaClient(url: "47.238.220.71:16110")
-        try await client.getBalancesByAddresses(addresses: ["kaspa:qrmynkncs7lxe34knahjtusztek3sqwpsea8443rentqqle6vglfx8s55enju","kaspa:qzfu7dvxeat9h6z07m4k7dkjsw6y3atpue4ejgwt4dxdm9wumepmgz8qcectr"]) { entries in
-            debugPrint(entries.count)
-            debugPrint("ddddddddddddd")
-        } failture: { error in
-            debugPrint(error)
-        }
+//        let client = KaspaClient(host: "47.238.220.71", port: 16110)
+////        try KaspaClient(url: "47.238.220.71:16110")
+//        try await client.getBalancesByAddresses(addresses: ["kaspa:qrmynkncs7lxe34knahjtusztek3sqwpsea8443rentqqle6vglfx8s55enju","kaspa:qzfu7dvxeat9h6z07m4k7dkjsw6y3atpue4ejgwt4dxdm9wumepmgz8qcectr"]) { entries in
+//            debugPrint(entries.count)
+//            debugPrint("ddddddddddddd")
+//        } failture: { error in
+//            debugPrint(error)
+//        }
     }
 }
