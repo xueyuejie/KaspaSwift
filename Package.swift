678d8b45c4a8fcf9776df5a4aa5c2e2739b66317
xueyuejie <358838943@qq.com>
2024-11-20 10:15:18 +0800

    backup

--- Package.swift
+++ Package.swift
@@ -1,14 +1,10 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KaspaSwift",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
