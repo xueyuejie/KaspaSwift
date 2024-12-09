3e9d811fa97680cbf143532dd7949bbad6e3303f
xueyuejie <358838943@qq.com>
2024-12-09 15:51:56 +0800

    Update Serialization.swift

--- Sources/KaspaSwift/Extensions/Serialization.swift
+++ Sources/KaspaSwift/Extensions/Serialization.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CryptoSwift
 
 protocol BinaryConvertible {
     static func +(lhs: Data, rhs: Self) -> Data
@@ -82,33 +83,5 @@ extension Data {
             fatalError("This switch statement should be exhaustive without default clause")
         }
         return VarInt(value)
-    }
-}
-
-extension Data {
-    init?(hex: String) {
-        let len = hex.count / 2
-        var data = Data(capacity: len)
-        for i in 0..<len {
-            let j = hex.index(hex.startIndex, offsetBy: i * 2)
-            let k = hex.index(j, offsetBy: 2)
-            let bytes = hex[j..<k]
-            if var num = UInt8(bytes, radix: 16) {
-                data.append(&num, count: 1)
-            } else {
-                return nil
-            }
-        }
-        self = data
-    }
-
-    var hex: String {
-        return reduce("") { $0 + String(format: "%02x", $1) }
-    }
-}
-
-extension String {
-    var reversedData: Data? {
-        return Data(hex: self).map { Data($0.reversed()) }
     }
 }
