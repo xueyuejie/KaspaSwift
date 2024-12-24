7eb88471fd835274714c4d693d0b5aa49d8b28e9
xueyuejie <358838943@qq.com>
2024-12-24 16:11:55 +0800

    Update TxInput.swift

--- Sources/KaspaSwift/Transaction/Type/TxInput.swift
+++ Sources/KaspaSwift/Transaction/Type/TxInput.swift
@@ -14,14 +14,16 @@ public class TxInput {
     public let sequence: UInt64
     public let sigOpCount: Int
     public let utxoEntry: KaspaUtxoEntry
+    public let redeemScript: Data?
 
-    public init(address: KaspaAddress, previousOutpoint: KaspaOutpoint, signatureScript: Data = Data() , sequence: UInt64, sigOpCount: Int, utxoEntry: KaspaUtxoEntry) {
+    public init(address: KaspaAddress, previousOutpoint: KaspaOutpoint, signatureScript: Data = Data() , sequence: UInt64, sigOpCount: Int, utxoEntry: KaspaUtxoEntry, redeemScript: Data? = nil) {
         self.address = address
         self.previousOutpoint = previousOutpoint
         self.signatureScript = signatureScript
         self.sequence = sequence
         self.sigOpCount = sigOpCount
         self.utxoEntry = utxoEntry
+        self.redeemScript = redeemScript
     }
     
     @MainActor
@@ -33,10 +35,15 @@ public class TxInput {
         guard let signature = try? TransactionUtil.signSchnorr(hash: serializedTransaction, privateKey: privateKey) else {
             return nil
         }
+        var signatureData = [UInt8]()
+        signatureData.append(contentsOf: signature)
+        signatureData.append(UInt8(SigHashType.sigHashAll.rawValue))
+        if let _redeemScript = self.redeemScript {
+            signatureData.append(contentsOf: _redeemScript)
+        }
         var signatureScript = [UInt8]()
-        signatureScript.append(UInt8(signature.count + 1))
+        signatureScript.append(UInt8(signatureData.count))
         signatureScript.append(contentsOf: signature)
-        signatureScript.append(UInt8(SigHashType.sigHashAll.rawValue))
         self.signatureScript.replaceSubrange(0..<signatureScript.count, with: signatureScript)
         return self
     }
